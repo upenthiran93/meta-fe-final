9fdb08f760d2f883ac5907a9bf5891ed1a3c77a5
upenthiran93 <upen@42interactive.com>
2024-05-11 10:41:58 +1000

    header react

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+      <footer>
+        <p>© 2022 Little Lemon</p>
+      </footer>
+    );
+  }
+  export default Footer;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+function Nav() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#menu">Menu</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    );
+}
+export default Nav;
