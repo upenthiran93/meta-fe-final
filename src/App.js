71a415db673dd9806d54f3207f5191056ec784b2
upenthiran93 <upen@42interactive.com>
2024-05-13 07:17:33 +1000

    router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
 import Header from './components/Header';
 import Nav from './components/Nav';
-import Main from './components/Main'; 
+import Main from './components/Main';
 import Footer from './components/Footer';
-
-
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
 
 function App() {
   return (
+
     <div className='App'>
-     <Header/>
-     <Main/>
-     <Footer/>
+
+        <Header />
+        <Router>
+        <Routes>
+            <Route path="/" element={<Main />} />
+
+        </Routes>
+        </Router>
+        <Footer />
 
     </div>
   );
