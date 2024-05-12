754b1e1e546c7491d0c2d19dd7991db3ac6c04e5
upenthiran93 <upen@42interactive.com>
2024-05-13 06:38:28 +1000

    footer

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style/TestimonialCard.css";
+import image from "../assets/images/profile1.jpg";
 
 function TestimonialCard() {
   return (
     <div className="testimonial_card">
       <div className="testimonial_card_image">
-        <img src="https://via.placeholder.com/150" alt="testimonial" />
+        <img src={image} alt="testimonial" />
       </div>
       <div className="testimonial_card_content">
         <p>
