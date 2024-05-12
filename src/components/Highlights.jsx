9feb315cea3d293b0537c0e783668ed84e447c55
upenthiran93 <upen@42interactive.com>
2024-05-12 12:38:13 +1000

    higlit fix

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style/Highlights.css';
+function Highlights () {
+  return (
+    <div className={"Highlights"}>
+      <h2>Highlights</h2>
+      <p>Some highlights of the event</p>
+    </div>
+  )
+}
+export default Highlights;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './style/Main.css';
 import Hero from "./Hero";
+import Highlights from "./Highlights";
 function Main() {
     return (
         <main className={"Main"}>
            <Hero/>
+            <Highlights/>
         </main>
     );
 }
