531ccc5ac5fb2813b1205fae52dee15d3154c298
upenthiran93 <upen@42interactive.com>
2024-05-13 19:53:10 +1000

    ini funtion

--- src/App.js
+++ src/App.js
@@ -16,10 +16,13 @@ function undateTime(times,action) {
         return time !== action;
     });
 }
+function InitialTime() {
 
+    return ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+}
 function App() {
 
-    const [times, setTimes] = React.useReducer (undateTime,['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']);
+    const [times, setTimes] = React.useReducer (undateTime,InitialTime());
     return (
 
         <div className='App'>
