20e86135e1476286769971ba3ee5759717ac175d
upenthiran93 <upen@42interactive.com>
2024-05-13 19:59:56 +1000

    booking

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import{fetchAPI, submitAPI} from './API/API';
 import {
     BrowserRouter as Router,
     Routes,
@@ -17,8 +18,10 @@ function undateTime(times,action) {
     });
 }
 function InitialTime() {
-
-    return ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+   var val = fetchAPI(new Date());
+   console.log(val);
+    // return ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+    return val;
 }
 function App() {
 
