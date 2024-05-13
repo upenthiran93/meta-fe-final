3f7db4a57bb1d12a00d1324b784fbd0716fa7d96
upenthiran93 <upen@42interactive.com>
2024-05-14 06:06:18 +1000

    Data fo special card

---
+++ src/API/SpecialCardData.js
@@ -0,0 +1,26 @@
+import lemonDessert from '../assets/images/lemon-dessert1.jpg';
+import greekSalad1 from '../assets/images/greek-salad1.jpg';
+import bruchetta from '../assets/images/bruchetta.svg';
+
+const specialData = [
+    {
+        title: "Bruchetta",
+        description: "Bruchetta is an Italian antipasto.  basil, and sometimes mozzarella.",
+        image: bruchetta,
+        price: "$20"
+    },
+    {
+        title: "Greek Salad",
+        description: "Greek Salad is a mix of tomatoes, It's seasoned with salt and oregano.",
+        image: greekSalad1,
+        price: "$25"
+    },
+    {
+        title: "Lemon Dessert",
+        description: "This Lemon Dessert is a It's made with fresh lemons, sugar, eggs, and cream.",
+        image: lemonDessert,
+        price: "$30"
+    }
+];
+
+export default specialData;
--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './style/Highlights.css';
 import SpecialCard from "./SpecialCard";
-import image from "../assets/images/restaurant.jpg";
+
+import specialData from "../API/SpecialCardData"
 function Highlights () {
+
   return (
     <div className={"Highlights"}>
         <div className={"TitleBox"}>
@@ -10,15 +12,14 @@ function Highlights () {
             <button> oder noww</button>
         </div>
        < div className={"HighlightsBox"}>
-           {[...Array(3)].map((_, index) => (
-              <SpecialCard
-                  key={index}
-                  description={" and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum'"}
-                    title={"Name of the dish"}
-                    image={image}
-                  Price={"$ 20"}
-              />
-
+           {specialData.map((item, index) => (
+               <SpecialCard
+                   key={index}
+                   description={item.description}
+                   title={item.title}
+                   image={item.image}
+                   Price={item.price}
+               />
            ))}
        </div>
 
