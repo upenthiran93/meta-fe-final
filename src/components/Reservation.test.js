5d7d3aacabc62c4a169cb9d1b08c4642671c89a0
upenthiran93 <upen@42interactive.com>
2024-05-13 20:28:06 +1000

    Test

--- src/components/Reservation.test.js
+++ src/components/Reservation.test.js
@@ -1,7 +1,18 @@
 import { render, screen,fireEvent } from "@testing-library/react";
-import { app } from '../App';
+import { App, } from '../App';
 import Reservation from './Reservation';
+import { fetchAPI } from '../API/API';
 
+
+
+jest.mock('../API/API', () => ({
+    fetchAPI: jest.fn(() => ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']),
+}));
+
+test('App component fetches available booking times on render', () => {
+    render(<App />);
+    expect(fetchAPI).toHaveBeenCalled();
+});
 test('Renders the Reservation heading', () => {
     render(<Reservation Times={[]} despatch={() => {}} />);
     const headingElement = screen.getByText("Reservations");
@@ -22,4 +33,9 @@ test('handleSubmit function removes the selected time', () => {
 
     // Check if the dispatch function was called with the correct argument
     expect(mockDispatch).toHaveBeenCalledWith('19:00');
+});
+test('updateTimes function removes the selected time', () => {
+    const times = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+    const newTimes = updateTimes(times, '19:00');
+    expect(newTimes).toEqual(['17:00', '18:00', '20:00', '21:00', '22:00']);
 });
