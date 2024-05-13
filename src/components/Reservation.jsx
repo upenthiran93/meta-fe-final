8ba854a9790ad13b1f6959e3ec55567cd9244356
upenthiran93 <upen@42interactive.com>
2024-05-13 20:49:53 +1000

    warn the user

--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -1,19 +1,35 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './style/Reservation.css';
 import { useNavigate } from 'react-router-dom';
-import {submitAPI} from '../API/API';
+import { submitAPI } from '../API/API';
 
-const Reservation = ({Times, despatch}) => {
+const Reservation = ({ Times, despatch }) => {
     useEffect(() => {
         console.log('Times:', Times);
-    },[Times]);
+    }, [Times]);
+
     const [date, setDate] = useState('');
     const [time, setTime] = useState('');
     const [guests, setGuests] = useState(1);
     const [occasion, setOccasion] = useState('');
+    const [formErrors, setFormErrors] = useState({});
+
     const navigate = useNavigate();
+
+    const isFormValid = date && time && guests && occasion;
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        if (!isFormValid) {
+            // If form is not valid, set form errors
+            setFormErrors({
+                date: !date,
+                time: !time,
+                guests: !guests,
+                occasion: !occasion
+            });
+            return;
+        }
         const formData = {
             date: date,
             time: time,
@@ -32,32 +48,39 @@ const Reservation = ({Times, despatch}) => {
     return (
         <section className="reservation">
             <div className="container">
-                <section className="reservation-wrap">
-                    <div className="form-wrap">
+                <article className="reservation-wrap">
+                    <header>
                         <h2>Reservations</h2>
-                        <form onSubmit={handleSubmit} style={{display: "grid", maxWidth: "200px", gap: "20px"}}>
-                            <label htmlFor="res-date">Choose date</label>
-                            <input type="date" id="res-date" value={date} onChange={(e) => setDate(e.target.value)} />
-                            <label htmlFor="res-time">Choose time</label>
-                            <select id="res-time" value={time} onChange={(e) => setTime(e.target.value)} >
-                                {Times.map((time) => {
-                                    return <option key={time} value={time}>{time}</option>
-                                })}
-                            </select>
-                            <label htmlFor="guests">Number of guests</label>
-                            <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={(e) => setGuests(e.target.value)} />
-                            <label htmlFor="occasion">Occasion</label>
-                            <select id="occasion" value={occasion} onChange={(e) => setOccasion(e.target.value)}>
-                                <option>Birthday</option>
-                                <option>Anniversary</option>
-                            </select>
-                            <input type="submit" value="Make Your reservation" />
-                        </form>
-                    </div>
-                    <div className="img-wrap">
+                    </header>
+                    <form onSubmit={handleSubmit} style={{ display: "grid", maxWidth: "200px", gap: "20px" }}>
+                        <label htmlFor="res-date">Choose date</label>
+                        <input type="date" id="res-date" value={date} onChange={(e) => setDate(e.target.value)} required />
+                        {formErrors.date && <p>Please select a date</p>}
 
-                    </div>
-                </section>
+                        <label htmlFor="res-time">Choose time</label>
+                        <select id="res-time" value={time} onChange={(e) => setTime(e.target.value)} required>
+                            <option value="">Select Time</option>
+                            {Times.map((time) => {
+                                return <option key={time} value={time}>{time}</option>
+                            })}
+                        </select>
+                        {formErrors.time && <p>Please select a time</p>}
+
+                        <label htmlFor="guests">Number of guests</label>
+                        <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={(e) => setGuests(e.target.value)} required />
+                        {formErrors.guests && <p>Please enter the number of guests</p>}
+
+                        <label htmlFor="occasion">Occasion</label>
+                        <select id="occasion" value={occasion} onChange={(e) => setOccasion(e.target.value)} required>
+                            <option value="">Select Occasion</option>
+                            <option>Birthday</option>
+                            <option>Anniversary</option>
+                        </select>
+                        {formErrors.occasion && <p>Please select an occasion</p>}
+
+                        <input type="submit" value="Make Your reservation" aria-label="On Click" />
+                    </form>
+                </article>
             </div>
         </section>
     );
