83b53da206fe19d54a5418025ce5ddf4f674e593
upenthiran93 <upen@42interactive.com>
2024-05-14 07:22:40 +1000

    confermation page

--- src/components/ConfirmedBooking.jsx
+++ src/components/ConfirmedBooking.jsx
@@ -1,8 +1,17 @@
+
+import { useNavigate } from 'react-router-dom';
+
 function ConfirmedBooking (props) {
+    const navigate = useNavigate();
+
     return (
-        <div>
+        <div className={"ConfirmedBooking"}>
             <h1>Booking Confirmed</h1>
             <p>Your booking for {props.bookingDate} has been confirmed. Thank you!</p>
+            <div className={"btnGroup"}>
+            <button onClick={() => navigate('/')}>Go to Home</button>
+            <button onClick={() => navigate('/reservation')}>Reservation</button>
+            </div>
         </div>
     )
 }
